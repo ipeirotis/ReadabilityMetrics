ab4edb32de34a8bacbe5f8cd2d62b246aba9b228
Panos Ipeirotis <ipeirotis@gmail.com>
2012-10-07 12:25:13 -0400

    Refactored MetricType to avoid the if-else statement.

--- src/com/ipeirotis/rest/readability/MetricType.java
+++ src/com/ipeirotis/rest/readability/MetricType.java
@@ -7,42 +7,17 @@ package com.ipeirotis.rest.readability;
  *
  */
 public enum MetricType {
-	SMOG(1), FLESCH_READING(2), FLESCH_KINCAID(3), ARI(4), GUNNING_FOG(5),
-	COLEMAN_LIAU(6), SMOG_INDEX(7);
+	SMOG, FLESCH_READING, FLESCH_KINCAID, ARI, GUNNING_FOG, COLEMAN_LIAU, SMOG_INDEX;
 
-	private Integer type = null;
-	private MetricType(Integer type) {
-		
-	}	
-	public Integer getType() {
-		return type;
-	}
-	
 	public static MetricType fromString(String value) {
-		value = value.toUpperCase();
-		if("SMOG".equals(value)) {
-			return MetricType.SMOG;
+
+		String v = value.toUpperCase().trim();
+		for (MetricType metric : MetricType.values()) {
+			if (metric.name().equals(v)) {
+				return metric;
+			}
 		}
-		else if("FLESCH_READING".equals(value)) {
-			return MetricType.FLESCH_READING;
-		}
-		else if("FLESCH_KINCAID".equals(value)) {
-			return MetricType.FLESCH_KINCAID;
-		}
-		else if("ARI".equals(value)) {
-			return MetricType.ARI;
-		}
-		else if("GUNNING_FOG".equals(value)) {
-			return MetricType.GUNNING_FOG;
-		}
-		else if("COLEMAN_LIAU".equals(value)) {
-			return MetricType.COLEMAN_LIAU;
-		}
-		else if("SMOG_INDEX".equals(value)) {
-			return MetricType.SMOG_INDEX;
-		}
-		else {
-			return null;
-		}
+		
+		throw new IllegalArgumentException("Incorrect readability metric:" + v);
 	}
 }
