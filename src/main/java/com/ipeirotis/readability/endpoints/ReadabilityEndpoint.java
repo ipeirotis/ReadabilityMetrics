5325306766c60a57ef68b1a8305cdb4adb7e291d
nkislitsin <nkislitsin@mail.ru>
2014-10-03 23:49:40 +0300

    removed TextDto

--- src/main/java/com/ipeirotis/readability/endpoints/ReadabilityEndpoint.java
+++ src/main/java/com/ipeirotis/readability/endpoints/ReadabilityEndpoint.java
@@ -4,10 +4,11 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.inject.Named;
+
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiMethod.HttpMethod;
-import com.ipeirotis.readability.dto.TextDto;
 import com.ipeirotis.readability.engine.Readability;
 import com.ipeirotis.readability.enums.MetricType;
 
@@ -15,9 +16,9 @@ import com.ipeirotis.readability.enums.MetricType;
 public class ReadabilityEndpoint {
 
     @ApiMethod(name = "getReadabilityMetrics", path = "getReadabilityMetrics", httpMethod = HttpMethod.POST)
-    public Map<MetricType, BigDecimal> get(TextDto text) {
+    public Map<MetricType, BigDecimal> get(@Named("text") String text) {
         Map<MetricType, BigDecimal> result = new HashMap<MetricType, BigDecimal>();
-        Readability r = new Readability(text.getText());
+        Readability r = new Readability(text);
 
         for (MetricType metricType : MetricType.values()) {            
             BigDecimal value = new BigDecimal(Double.toString(r.getMetric(metricType)));
